5af81fd9fe82f8761041a3fee84d9975a16d25ab
Johannes Millan <johannes.millan@gmail.com>
2016-02-16 23:23:30 +0000

    fix for dasherizing pathes

--- sub-generator-base.js
+++ sub-generator-base.js
@@ -125,7 +125,7 @@ module.exports = yeoman.generators.NamedBase.extend({
     {
         var style = this.config.get('pathOutputStyle') || 'dasherize';
         // first letter to lowercase to prevent issues with dasherize
-        name = name.charAt(0).toUpperCase() + name.slice(1);
+        name = name.charAt(0).toLowerCase() + name.slice(1);
         return _s[style](name);
     },
 
