c47a08048af717f0021440a0f8143962bc216895
Johannes Millan <johannes.millan@gmail.com>
2016-02-16 02:54:40 +0000

    add base state

--- templates/app/aaal-routes.js
+++ templates/app/aaal-routes.js
@@ -13,8 +13,14 @@ angular.module('<%= moduleName %>')
         'use strict';
 
         var AAAL_BASE_STATE = '<%= baseState %>';
+        var aaalBaseWithoutDot = AAAL_BASE_STATE.substring(0, AAAL_BASE_STATE.length - 1);
 
         $stateProvider
+            .state(aaalBaseWithoutDot, {
+                url: '/private',
+                abstract: true,
+                template: '<main ui-view></main>'
+            })
             /* STATES-NEEDLE - DO NOT REMOVE THIS */;
 
     });
