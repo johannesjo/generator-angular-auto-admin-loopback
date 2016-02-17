01e3d7bf51f0e9ab6235f03db525585e042ae195
Johannes Millan <johannes.millan@gmail.com>
2016-02-17 02:07:41 +0000

    finished object to string converter

--- helper.js
+++ helper.js
@@ -39,3 +39,82 @@ exports.injectRoute = function (routesFile, name, url, tplUrl, ctrl, that)
     that.log.writeln(chalk.green(' updating') + ' %s', path.basename(routesFile));
 };
 
+
+exports.simpleObjectToString = simpleObjectToString;
+
+
+function simpleObjectToString(obj, startIndent)
+{
+    var IND = '    ';
+
+    function handleTypes(item, startIndent)
+    {
+        var str = '';
+        if (typeof(item) === "boolean") {
+            str += item ? 'true' : 'false';
+        } else if (typeof item === 'number') {
+            str += item;
+        } else if (typeof item === 'string') {
+            str += '\'' + item + '\'';
+        } else if (item instanceof Array) {
+            str += createArrayStr(item, startIndent + IND);
+        } else if (typeof item === 'object') {
+            str += createObjStr(item, startIndent + IND);
+        }
+        return str;
+    }
+
+    function createArrayStr(arr, startIndent)
+    {
+        var arrStr = '[';
+        for (var i = 0; i < arr.length; i++) {
+            var arrItem = arr[i];
+            arrStr += '\n' + startIndent + IND;
+            arrStr += handleTypes(arrItem, startIndent);
+
+            if (i < arr.length - 1) {
+                arrStr += ',';
+            }
+        }
+        arrStr += '\n' + startIndent + ']';
+
+        return arrStr;
+    }
+
+    function createObjectPropsStr(obj, key, startIndent, i)
+    {
+        var objPropsStr = '';
+        var prop = obj[key];
+        var objLength = Object.keys(obj).length - 1;
+
+        objPropsStr += '\n' + startIndent + IND + '\'' + key + '\': ';
+        objPropsStr += handleTypes(prop, startIndent);
+
+        if (i < objLength) {
+            objPropsStr += ',';
+        }
+
+        return objPropsStr;
+    }
+
+    function createObjStr(obj, startIndent)
+    {
+        var str = '';
+        var i = 0;
+        str += '{';
+        for (var key in obj) {
+            str += createObjectPropsStr(obj, key, startIndent, i);
+            i++;
+        }
+        str += '\n' + startIndent + '}';
+        return str;
+    }
+
+
+    if (obj instanceof Array) {
+        return createArrayStr(obj, startIndent);
+    } else {
+        return createObjStr(obj, startIndent);
+    }
+}
+
