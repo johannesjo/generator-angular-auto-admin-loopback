06e9a85d690110fb141623de0bcb15a96d58d195
Johannes Millan <johannes.millan@gmail.com>
2016-02-17 17:44:27 +0000

    fix view

--- templates/app/view.js
+++ templates/app/view.js
@@ -6,15 +6,14 @@
  * Controller of the <%= moduleName %>
  */
 angular.module('<%= moduleName %>')
-    //.controller('<%= classedName %><%= nameSuffix %>', function myFunc($scope, $state, <%= modelServiceName %>) {
-    //    'use strict';
-    //
-    //    var ModelService = <%= modelServiceName %>;
-
-    .controller('<%= classedName %><%= nameSuffix %>', function myFunc($scope, $state, TestModel) {
+    .controller('<%= classedName %><%= nameSuffix %>', function myFunc($scope, $state, <%= modelServiceName %>) {
         'use strict';
 
-        var ModelService = TestModel;
+        var ModelService = <%= modelServiceName %>;
+    //
+    //.controller('<%= classedName %><%= nameSuffix %>', function myFunc($scope, $state, TestModel) {
+    //    'use strict';
+        //var ModelService = TestModel;
 
         if ($state.params.id) {
             $scope.model = ModelService.findById({id: $state.params.id});
