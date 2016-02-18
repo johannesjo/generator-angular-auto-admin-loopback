417d321a34f2a0cb4a5c04029f0a2cd2f1556f8e
Johannes Millan <johannes.millan@gmail.com>
2016-02-18 23:29:37 +0000

    reorder edit controller

--- templates/app/edit.js
+++ templates/app/edit.js
@@ -18,16 +18,6 @@
         var vm = this;
         var ModelService = <%= modelServiceName %>;
 
-
-        function postSave() {
-            ngToast.create('Saved');
-            $state.go('<%= overviewStateFull %>');
-        }
-
-        if ($state.params.id) {
-            vm.model = ModelService.findById({id: $state.params.id});
-        }
-
         //form and schema definition
         vm.schema = aaalToSchemaForm('<%= modelServiceName %>');
         vm.form = [
@@ -37,6 +27,19 @@
                 title: 'Save'
             }
         ];
+
+        // load or create new instance
+        if ($state.params.id) {
+            vm.model = ModelService.findById({id: $state.params.id});
+        } else {
+            vm.model = {};
+        }
+
+
+        function postSave() {
+            ngToast.create('Saved');
+            $state.go('<%= overviewStateFull %>');
+        }
 
         vm.createOrUpdate = function() {
             // update
