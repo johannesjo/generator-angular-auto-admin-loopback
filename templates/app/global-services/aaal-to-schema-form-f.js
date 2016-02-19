bf765f2002e80ce837ed8eedc7ffeb8ff60aadfb
Johannes Millan <johannes.millan@gmail.com>
2016-02-19 01:19:38 +0000

    enable editing of arrays

--- templates/app/global-services/aaal-to-schema-form-f.js
+++ templates/app/global-services/aaal-to-schema-form-f.js
@@ -10,8 +10,17 @@
     'use strict';
 
     angular
-        .module('<%= moduleName %>')
+        .module('aaal')
         .factory('aaalToSchemaForm', aaalToSchemaForm);
+
+    var PROPERTIES_TO_COPY = [
+        'type',
+        'required'
+    ];
+    var PROPERTIES_TO_TRANSFORM = [];
+
+    var transformations = {};
+
 
     /* @ngInject */
     function aaalToSchemaForm(aaalModelDefinitions) {
@@ -25,22 +34,72 @@
                 properties: {}
             };
 
-            for (var key in model.properties) {
-                var modelProperty = model.properties[key];
-                console.log(key, modelProperty);
-                schema.properties[key] = {
-                    type: modelProperty.type,
-                    title: key
-                };
-
-            }
-
-            console.log(schema);
+            schema.properties = processModelProperties(model.properties, schema.properties);
 
             return schema;
         }
+
+
+        function processPropertyProperties(modelProperty) {
+            var schemaPropertyProperties = {};
+            for (var key in modelProperty) {
+                if (modelProperty.hasOwnProperty(key)) {
+                    var modelPropertyProperty = modelProperty[key];
+
+                    if (PROPERTIES_TO_COPY.indexOf(key) > -1) {
+                        schemaPropertyProperties[key] = modelPropertyProperty;
+                    } else if (PROPERTIES_TO_TRANSFORM.indexOf(key) > -1) {
+                        schemaPropertyProperties[key] = transformations[key](modelPropertyProperty);
+                    }
+                }
+            }
+
+            // handle arrays
+            if (modelProperty.type instanceof Array) {
+                schemaPropertyProperties.type = 'array';
+                delete schemaPropertyProperties.required;
+                schemaPropertyProperties.items = processArrayDefinitions(modelProperty.type);
+            }
+
+            return schemaPropertyProperties;
+        }
+
+
+        function processModelProperties(modelProperties, schemaProperties) {
+            for (var key in modelProperties) {
+                if (modelProperties.hasOwnProperty(key)) {
+                    var modelProperty = modelProperties[key];
+                    schemaProperties[key] = processPropertyProperties(modelProperty);
+                }
+            }
+
+            return schemaProperties;
+        }
+
+
+        function processArrayDefinitions(arrayTypeDefinition) {
+            var schemaDef = {};
+            for (var i = 0; i < arrayTypeDefinition.length; i++) {
+                var def = arrayTypeDefinition[i];
+                if (def === 'string') {
+                    schemaDef.type = 'string';
+                    break;
+                } else if (typeof def === 'object') {
+                    if (!schemaDef.type) {
+                        schemaDef.type = 'object';
+                        schemaDef.properties = {};
+                        schemaDef.required = [];
+                    }
+                    schemaDef.properties[def.name] = processPropertyProperties(def);
+                    if (schemaDef.properties[def.name].required) {
+                        // this leads to weird behaviour of the schema form...
+                        //schemaDef.required.push(def.name);
+                        delete schemaDef.properties[def.name].required;
+                    }
+                }
+            }
+
+            return schemaDef;
+        }
     }
-
-
 })();
-
